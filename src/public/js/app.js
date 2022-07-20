0e3be5f53ceb1c10b96e39fd963a3a79296d6d85
hong <hong_1598@naver.com>
2022-07-21 01:06:36 +0900

    웹소켓 구현

--- src/public/js/app.js
+++ src/public/js/app.js
@@ -1,2 +1,18 @@
-alert("hi!");
-console.log("Q!!!!");
+//const socket = new WebSocket("http://localhost:3000"); // 이건 http 호출이라 반응 X
+const socket = new WebSocket(`ws://${window.location.host}`); // window.location은 접속한 장소의 호스트를 반환해주는 속성
+
+socket.addEventListener("open", () => {
+  console.log("connected to Server");
+});
+
+socket.addEventListener("message", (message) => {
+  console.log("Just got this", message.data, "from the server");
+});
+
+socket.addEventListener("close", () => {
+  console.log("Disconnected from Server");
+});
+
+setTimeout(() => {
+  socket.send("hello from browser!");
+}, 5000);
--- src/server.js
+++ src/server.js
@@ -1,3 +1,5 @@
+import http from "http";
+import WebSocket from "ws";
 import express from "express";
 
 const app = express();
@@ -11,4 +13,24 @@ app.get("/", (req, res) => res.render("home"));
 app.get("/*", (req, res) => res.redirect("/"));
 
 const handleListen = () => console.log("Listening on http://localhost:3000");
-app.listen(3000, handleListen);
+const server = http.createServer(app); // 노드 js의 http 패키지로 express로 만든 서버 애플리케이션 제공 http서버 생성
+const wss = new WebSocket.Server({ server }); // 웹소켓 서버 생성
+
+// function handleConnection(socket) {
+//   console.log(socket);
+// }
+
+// wss.on("connection", handleConnection);
+
+wss.on("connection", (socket) => {
+  //익명함수로 표현하기
+  // console.log(socket);
+  console.log("Connected to Browser");
+  socket.on("close", () => console.log("Disconnected from Browser"));
+  socket.on("message", (message) => {
+    console.log(`${message}`);
+  });
+  socket.send("hello!");
+});
+
+server.listen(3000, handleListen);
